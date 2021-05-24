1557884201efc2c9ac1af08063c4fd1b102fec19
Iliaavilov <ieavilov@yandex.ru>
2021-05-24 20:11:17 +0300

    Добавил skf валидацию

--- cv.py
+++ cv.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import StratifiedKFold
 
 def get_indices_ts(data, splits, date_col):
     '''Time series cross validation
@@ -20,4 +21,25 @@ def get_indices_ts(data, splits, date_col):
         test = data[(data[date_col] >= pd.to_datetime(split[0])) &
                     (data[date_col] < pd.to_datetime(split[1]))].index.values
         cv.append((train, test))
+    return(cv)
+
+
+
+def get_indices_skf(X, y, n_splits, random_state):
+    '''
+    :param X: фичи для обучения модели
+    :type X: numpy 2d array or pandas dataframe
+    :param y: таргет для обучения модели
+    :type y: numpy array or pandas dataframe
+    :param n_splits: количество фолдов
+    :type n_splits: int
+    :param random_state: andom seed
+    :type random_state: int
+    :return:
+    :rtype:
+    '''
+
+
+    skf = StratifiedKFold(n_splits = n_splits, random_state = random_state, shuffle = True)
+    cv = list(skf.split(X, y))
     return(cv)
